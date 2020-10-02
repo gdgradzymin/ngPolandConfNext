8387482130a91fcfa925c4292350b88c137ebc4c
Eyzo280 <48450982+Eyzo280@users.noreply.github.com>
2020-10-02 13:35:15 +0200

    fix sidemenu highlight

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -29,141 +29,147 @@ class _DrawerNgState extends State<DrawerNg> {
           ),
           Consumer<SelectedPage>(
             builder: (context, selectedPage, _) {
-              double _animationTop = selectedPage.getPage.number *
-                  MediaQuery.of(context).size.height *
-                  0.07;
+              Color _shadowColor = Theme.of(context).dividerColor;
 
-              return Stack(
+              return Column(
                 children: [
-                  AnimatedPositioned(
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.easeIn,
-                    top: _animationTop,
-                    height: MediaQuery.of(context).size.height * 0.07,
-                    child: Container(
-                      width: MediaQuery.of(context).size.height * 0.75,
-                      color: Theme.of(context).dividerColor,
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.home
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.home
+                          ? true
+                          : false,
+                      leading: const Icon(
+                        FontAwesomeIcons.gripVertical,
+                      ),
+                      title: const Text('Home'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.home);
+                        Navigator.of(context).popAndPushNamed(Home.routeName);
+                      },
                     ),
                   ),
-                  Positioned(
-                    child: Column(
-                      children: [
-                        ListTile(
-                          selected: selectedPage.getPage.name == PagesName.home
-                              ? true
-                              : false,
-                          leading: Icon(
-                            FontAwesomeIcons.gripVertical,
-                          ),
-                          title: Text('Home'),
-                          onTap: () {
-                            selectedPage.changeSelected(name: PagesName.home);
-                            Navigator.of(context)
-                                .popAndPushNamed(Home.routeName);
-                          },
-                        ),
-                        ListTile(
-                          selected:
-                              selectedPage.getPage.name == PagesName.schedule
-                                  ? true
-                                  : false,
-                          leading: Icon(
-                            FontAwesomeIcons.solidClock,
-                          ),
-                          title: Text('Schedule'),
-                          onTap: () {
-                            selectedPage.changeSelected(
-                                name: PagesName.schedule);
-                            Navigator.of(context)
-                                .popAndPushNamed(Schedule.routeName);
-                          },
-                        ),
-                        ListTile(
-                          selected:
-                              selectedPage.getPage.name == PagesName.workshops
-                                  ? true
-                                  : false,
-                          leading: Icon(
-                            FontAwesomeIcons.solidKeyboard,
-                          ),
-                          title: Text('Workshops'),
-                          onTap: () {
-                            selectedPage.changeSelected(
-                                name: PagesName.workshops);
-                            Navigator.of(context)
-                                .popAndPushNamed(WorkShops.routeName);
-                          },
-                        ),
-                        ListTile(
-                          selected:
-                              selectedPage.getPage.name == PagesName.ngGirls
-                                  ? true
-                                  : false,
-                          leading: Icon(
-                            FontAwesomeIcons.female,
-                            size: 30,
-                          ),
-                          title: Text('ngGirls'),
-                          onTap: () {
-                            selectedPage.changeSelected(
-                                name: PagesName.ngGirls);
-                            Navigator.of(context)
-                                .popAndPushNamed(NgGirls.routeName);
-                          },
-                        ),
-                        ListTile(
-                          selected:
-                              selectedPage.getPage.name == PagesName.speakers
-                                  ? true
-                                  : false,
-                          leading: Icon(
-                            FontAwesomeIcons.users,
-                            size: 23,
-                          ),
-                          title: Text('Speakers'),
-                          onTap: () {
-                            selectedPage.changeSelected(
-                                name: PagesName.speakers);
-                            Navigator.of(context)
-                                .popAndPushNamed(Speakers.routeName);
-                          },
-                        ),
-                        ListTile(
-                          selected: selectedPage.getPage.name == PagesName.info
-                              ? true
-                              : false,
-                          leading: Icon(
-                            FontAwesomeIcons.info,
-                          ),
-                          title: Text('Info'),
-                          onTap: () {
-                            selectedPage.changeSelected(name: PagesName.info);
-                            Navigator.of(context)
-                                .popAndPushNamed(Info.routeName);
-                          },
-                        ),
-                        ListTile(
-                          selected: selectedPage.getPage.name == PagesName.about
-                              ? true
-                              : false,
-                          leading: Icon(
-                            FontAwesomeIcons.code,
-                          ),
-                          title: Text('About'),
-                          onTap: () {
-                            selectedPage.changeSelected(name: PagesName.about);
-                            Navigator.of(context)
-                                .popAndPushNamed(About.routeName);
-                          },
-                        ),
-                        Divider(
-                          thickness: 2,
-                          height: 25,
-                        ),
-                        SwitchDarkMode(),
-                      ],
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.schedule
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.schedule
+                          ? true
+                          : false,
+                      leading: Icon(
+                        FontAwesomeIcons.solidClock,
+                      ),
+                      title: const Text('Schedule'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.schedule);
+                        Navigator.of(context)
+                            .popAndPushNamed(Schedule.routeName);
+                      },
                     ),
                   ),
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.workshops
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.workshops
+                          ? true
+                          : false,
+                      leading: const Icon(
+                        FontAwesomeIcons.solidKeyboard,
+                      ),
+                      title: const Text('Workshops'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.workshops);
+                        Navigator.of(context)
+                            .popAndPushNamed(WorkShops.routeName);
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.ngGirls
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.ngGirls
+                          ? true
+                          : false,
+                      leading: const Icon(
+                        FontAwesomeIcons.female,
+                        size: 30,
+                      ),
+                      title: const Text('ngGirls'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.ngGirls);
+                        Navigator.of(context)
+                            .popAndPushNamed(NgGirls.routeName);
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.speakers
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.speakers
+                          ? true
+                          : false,
+                      leading: const Icon(
+                        FontAwesomeIcons.users,
+                        size: 23,
+                      ),
+                      title: const Text('Speakers'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.speakers);
+                        Navigator.of(context)
+                            .popAndPushNamed(Speakers.routeName);
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.info
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.info
+                          ? true
+                          : false,
+                      leading: const Icon(
+                        FontAwesomeIcons.info,
+                      ),
+                      title: const Text('Info'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.info);
+                        Navigator.of(context).popAndPushNamed(Info.routeName);
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: selectedPage.getPage.name == PagesName.about
+                        ? _shadowColor
+                        : Colors.transparent,
+                    child: ListTile(
+                      selected: selectedPage.getPage.name == PagesName.about
+                          ? true
+                          : false,
+                      leading: const Icon(
+                        FontAwesomeIcons.code,
+                      ),
+                      title: const Text('About'),
+                      onTap: () {
+                        selectedPage.changeSelected(name: PagesName.about);
+                        Navigator.of(context).popAndPushNamed(About.routeName);
+                      },
+                    ),
+                  ),
+                  Divider(
+                    thickness: 2,
+                    height: 25,
+                  ),
+                  SwitchDarkMode(),
                 ],
               );
             },
