80c9a89352e69de233fd2e3732fedf4080dadcf8
Eyzo280 <48450982+Eyzo280@users.noreply.github.com>
2020-10-08 19:11:14 +0200

    Added workShops, backend to speakers and code fix

--- lib/screens/speakerDetails.dart
+++ lib/screens/speakerDetails.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:ng_poland_conf_next/models/contentful.dart';
 import 'package:ng_poland_conf_next/providers/themeManager.dart';
 import 'package:provider/provider.dart';
 
 class SpeakerDetails extends StatelessWidget {
   static const routeName = '/SpeakerDetails';
 
+  Widget _flightShuttleBuilder(
+    BuildContext flightContext,
+    Animation<double> animation,
+    HeroFlightDirection flightDirection,
+    BuildContext fromHeroContext,
+    BuildContext toHeroContext,
+  ) {
+    return DefaultTextStyle(
+      style: DefaultTextStyle.of(toHeroContext).style,
+      child: toHeroContext.widget,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Speaker _speaker = ModalRoute.of(context).settings.arguments as Speaker;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'speakerName', // speaker.name
-          style: TextStyle(
-            color: Provider.of<ThemeNotifier>(context).darkTheme
-                ? Theme.of(context).accentColor
-                : Colors.white,
+        title: Hero(
+          tag: 'name' + _speaker.name,
+          flightShuttleBuilder: _flightShuttleBuilder,
+          child: Text(
+            _speaker.name,
+            style: TextStyle(
+              color: Provider.of<ThemeNotifier>(context).darkTheme
+                  ? Theme.of(context).accentColor
+                  : Colors.white,
+            ),
           ),
         ),
         centerTitle: true,
@@ -28,23 +48,38 @@ class SpeakerDetails extends StatelessWidget {
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Image.asset(
-                'assets/images/person.png',
-                width: MediaQuery.of(context).size.width * 0.5,
+              child: Hero(
+                tag: 'image' + _speaker.name,
+                child: _speaker.photoFileUrl == null
+                    ? Image.asset(
+                        'assets/images/person.png',
+                        width: MediaQuery.of(context).size.width * 0.5,
+                      )
+                    : Image.network(
+                        'http:${_speaker.photoFileUrl}',
+                        width: MediaQuery.of(context).size.width * 0.5,
+                      ),
               ),
             ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.02,
             ),
-            Text(
-              'speakerRole', // speaker.role
+            Hero(
+              tag: 'role' + _speaker.name,
+              flightShuttleBuilder: _flightShuttleBuilder,
+              child: Text(
+                _speaker.role,
+                style: Theme.of(context).textTheme.bodyText2.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
             ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.02,
             ),
             Container(
               alignment: Alignment.centerLeft,
-              child: Text('speakerBio'), // speaker.bio
+              child: Text(_speaker.bio),
             ),
           ],
         ),
