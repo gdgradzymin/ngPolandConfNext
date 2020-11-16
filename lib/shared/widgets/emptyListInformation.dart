de327a8e8a78069246e78d78d564af97a5745ca1
Sebastian Denis <sebastian.denis@outlook.com>
2020-11-16 02:10:15 +0100

    empty list refresh fixed

--- lib/shared/widgets/emptyListInformation.dart
+++ lib/shared/widgets/emptyListInformation.dart
@@ -3,27 +3,34 @@ import 'package:flutter/material.dart';
 class EmptyListInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Icon(
-          Icons.update,
-          size: 85,
-          color: Theme.of(context).accentColor.withOpacity(0.5),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Text(
-            'Sorry, no results returned. Please check again later.',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyText2.copyWith(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(
+                Icons.update,
+                size: 85,
+                color: Theme.of(context).accentColor.withOpacity(0.5),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Text(
+                  'Sorry, no results returned. Please check again later.',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
                 ),
-          ),
-        ),
-      ],
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
